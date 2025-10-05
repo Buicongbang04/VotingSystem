f6ded3fd207be31ffd9c2b2529fd3dcf92753da4
Buicongbang04 <buicongbang1010@gmail.com>
2025-10-05 12:01:00 -0400

    fix: page hero

--- src/components/landingPart/HeroPage.tsx
+++ src/components/landingPart/HeroPage.tsx
@@ -43,36 +43,31 @@ const HeroPage = () => {
       '
     >
       <div className='flex flex-col items-start justify-start -translate-y-30'>
-        <div
-                  className='
-                    flex justify-start md:justify-start items-start
-                    pt-[4vh] sm:pt-[6vh] md:pt-[6vh] lg:pt-[6vh] xl:pt-[2vh]
-                  '
-                >
-                  <Image
-                    src='/images/hero.png'
-                    alt='logo'
-                    width={877}
-                    height={200}
-                    priority
-                    draggable={false}
-                    className='
-                      h-auto
-                      w-[80vw] max-w-[520px]
-                      sm:w-[70vw] sm:max-w-[640px]
-                      md:w-[55vw] md:max-w-[720px]
-                      lg:w-[700px] lg:max-w-[700px]
-                      xl:w-[877px] xl:max-w-[877px]
-                    '
-                    sizes='
-                      (min-width:1280px) 877px,
-                      (min-width:1024px) 820px,
-                      (min-width:768px) 55vw,
-                      (min-width:640px) 70vw,
-                      80vw
-                    '
-                  />
-                </div>
+        <div className='ml-auto'>
+          <Image
+            src='/images/hero.png'
+            alt='logo'
+            width={877}
+            height={200}
+            priority
+            draggable={false}
+            className='
+                        h-auto
+                        w-[80vw] max-w-[520px]
+                        sm:w-[70vw] sm:max-w-[640px]
+                        md:w-[55vw] md:max-w-[720px]
+                        lg:w-[700px] lg:max-w-[700px]
+                        xl:w-[877px] xl:max-w-[877px]
+                      '
+            sizes='
+                        (min-width:1280px) 877px,
+                        (min-width:1024px) 820px,
+                        (min-width:768px) 55vw,
+                        (min-width:640px) 70vw,
+                        80vw
+                      '
+          />
+        </div>
 
         <div
           className='
